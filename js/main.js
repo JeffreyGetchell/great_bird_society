819e2b5a19344e68ebe4e2ffd253e490e58880e2
JeffreyGetchell <flyingetch@aol.com>
2018-03-11 16:01:13 -0400

    main menu creation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-// Make it rain!
+//  <.....hamburger nav menu.......>
+
+$('div.hamburger').on('click', function(){
+  $('.sidebar').toggleClass('active');
+  $('div.hamburger').fadeOut("slow", 0.15);
+});
+
+$('.close').on('click', function(){
+   $('.sidebar').removeClass('active');
+});
