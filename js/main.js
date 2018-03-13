03e83058097c6fadd1e26cc458be2c8378703f41
JeffreyGetchell <flyingetch@aol.com>
2018-03-13 14:40:09 -0400

    main image changes w/scroll

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ $('div.hamburger').on('click', function(){
 $('.close').on('click', function(){
    $('.sidebar').removeClass('active');
 });
+
+// main img is going to transition from 500px to a smaller size of 200px,
+// when user scrolls down
+$(window).on('scroll', function () {
+  var distanceScrolled = $(window).scrollTop();
+  // if the distanceScrolled is  greater than num, > make the image smaller
+  if(distanceScrolled > 200){
+    $('img').addClass('smaller-finch');
+  } else {
+    $('img').removeClass('smaller-finch');
+  }
+});
